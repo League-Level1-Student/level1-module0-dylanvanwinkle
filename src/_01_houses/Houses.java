7c0977561a2602a153daa544350efaef673b29de
dylanvanwinkle <dylanvanwinkle@gmail.com>
2020-04-10 13:32:59 -0700

    HousesIP

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,11 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
-		
+		Robot r = new Robot("batman");
+r.setX(800);
+r.setY(400);
 	}
 }
