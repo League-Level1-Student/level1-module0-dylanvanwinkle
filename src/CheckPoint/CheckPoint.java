7ba00f42e287fd5b08950abfb8894deef317e7f0
dylanvanwinkle <dylanvanwinkle@gmail.com>
2020-05-20 13:29:23 -0700

    1

--- src/CheckPoint/CheckPoint.java
+++ src/CheckPoint/CheckPoint.java
@@ -1,5 +1,40 @@
 package CheckPoint;
 
-public class CheckPoint {
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.JFrame;
+
+public class CheckPoint implements KeyListener {
+	public static void main(String[] args) {
+		new CheckPoint().run();
+	}
+
+	public void run() {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(600, 600));
+		frame.addKeyListener(this);
+		frame.pack();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("ouch");
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
